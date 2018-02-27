8bb2ff72bd64002daa945be9f50c48ba644c91ae
Robin Knipe <robin.knipe@gmail.com>
2018-02-27 18:27:17 +0000

    Update mock to better reflect the ServiceNow API

--- test/snow-api-mock/rest-api.js
+++ test/snow-api-mock/rest-api.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const { expect } = require('chai').use(require('chai-json'));
-process.title    = 'ServiceNow-mock-api';
+const moment      = require('moment');
+const { expect }  = require('chai').use(require('chai-json'));
+process.title     = 'ServiceNow-mock-api';
+const validFuture = str => (date => date.isValid() && date.isAfter(moment()))(moment(str, 'YYYY-MM-DD HH:mm:ss'));
+const newID       = 'CHG1234567';
 
 module.exports = {
   path:   '/api/now/table/x_fho_siam_integra_transactions',
@@ -10,12 +13,13 @@ module.exports = {
       res.status(401);
       res.send('unauthorised');
     } else {
+      res.status(201);
       next();
     }
   },
   method: 'POST',
   template: (params, query, body, cookies, headers) => {
-    let response = 'status updated';
+    let response;
 
     expect(headers).to.include({
       'content-type': 'application/json',
@@ -27,11 +31,13 @@ module.exports = {
     expect(body).to.have.property('messageid');
 
     if (body.messageid === 'HO_SIAM_IN_REST_CHG_POST_JSON') {
-      expect(body, `body has keys: ${body && body.payload ? Object.keys(JSON.parse(body.payload)) : 'no payload'}`)
-        .to.have.property('payload').that.is.a('string')
-        .and.that.is.json.that.includes.keys(['title', 'endTime', 'description', 'supplierRef']);
+      expect(body).to.have.property('payload').that.is.a('string');
+      const json = JSON.parse(body.payload);
+      expect(json).to.be.an('object').that.includes.keys(['title', 'endTime', 'description', 'supplierRef']);
       response = {
-        'internal_identifier': 'new int ID'
+        result: {
+          'internal_identifier': validFuture(json.endTime) ? newID : ''
+        }
       };
     } else {
       expect(body).to.have.property('messageid', 'HO_SIAM_IN_REST_CHG_UPDATE_JSON');
@@ -39,6 +45,12 @@ module.exports = {
       expect(body).to.have.property('external_identifier').that.is.a('string');
       expect(body).to.have.property('payload').that.is.a('string')
         .and.that.is.json.that.has.keys(['success', 'comments']);
+      response = {
+        result: {
+          status: !body.internal_identifier || !body.external_identifier ||
+            body.internal_identifier.startsWith('!') || body.external_identifier.startsWith('!') ? '7' : '5'
+        }
+      };
     }
 
     return response;
