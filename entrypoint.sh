e382d1a44b13a6043266db641478e5af98554911
Robin Knipe <robin.knipe@gmail.com>
2018-03-12 17:32:58 +0000

    Make sure the snow internal ID file exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+# make sure the internal ID file exists if it is supplied
+function affirm() {
+    if [ -n "$1" ] ; then
+        export PLUGIN_INTERNAL_ID_FILE="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+        touch "${PLUGIN_INTERNAL_ID_FILE}"
+    fi
+}
+
+affirm "${PLUGIN_INTERNAL_ID_FILE:-$SNOW_INT_ID_FILE}"
+
 cd /app/
 ./index.js
