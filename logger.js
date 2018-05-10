b04610becb634fe157f76b8ad683c3b5d228379a
Robin Knipe <robin.knipe@gmail.com>
2018-05-10 16:57:14 +0100

    Allow log level to be set by env var and CLI args
    ...as well as Drone plugin config settings.

--- logger.js
+++ logger.js
@@ -1,11 +1,23 @@
 'use strict';
 
+const yargs = require('yargs');
 const winston = require('winston');
 const { combine, timestamp, printf } = winston.format;
 
-const format = printf(info => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`);
+const format = printf(contents => `${contents.timestamp} ${contents.level.toUpperCase()}: ${contents.message}`);
+
+const inputOptions = yargs
+  .option({
+    'log-level': {
+      default: 'info',
+      coerce:  level => level.toLowerCase()
+    }
+  });
+const defaults = inputOptions.env('SNOW').argv;
+const options  = inputOptions.default(defaults).env('PLUGIN').argv;
 
 const logger = winston.createLogger({
+  level:      options['log-level'],
   format:     combine(timestamp(), format),
   transports: [ new winston.transports.Console() ]
 });
--- test/logger.js
+++ test/logger.js
@@ -18,12 +18,71 @@ describe('Logger', function () {
     it('should log errors to stderr', () =>
       expect(this.result)
         .to.have.property('stderr')
-        .that.contains(` ERROR: ${logs.error}`));
+        .that.contains(` ERROR: ${logs.error}`)
+        .and.does.not.contain(` DEBUG: ${logs.debug}`));
     it('should log warnings and info messages to stdout', () =>
       expect(this.result)
         .to.have.property('stdout')
         .that.contains(` WARN: ${logs.warn}`)
-        .and.contains(` INFO: ${logs.info}`));
+        .and.contains(` INFO: ${logs.info}`)
+        .and.does.not.contain(` VERBOSE: ${logs.verbose}`))
+  });
+
+  describe('configuring the log level to', () => {
+    describe('display errors only with an environment variable', function () {
+      before(() => {
+        this.result = shell.exec('SNOW_LOG_LEVEL=ERROR ./log-tester.js');
+        expect(this.result).to.include.property('code', 0);
+      });
+
+      it('should log only errors to stderr', () =>
+        expect(this.result)
+          .to.have.property('stderr')
+          .that.contains(` ERROR: ${logs.error}`)
+          .and.does.not.contain(` DEBUG: ${logs.debug}`));
+      it('should not log any messages to stdout', () =>
+        expect(this.result)
+          .to.have.property('stdout')
+          .that.is.empty);
+    });
+
+    describe('display warnings with drone plugin settings', function () {
+      before(() => {
+        this.result = shell.exec('PLUGIN_LOG_LEVEL=waRN ./log-tester.js');
+        expect(this.result).to.include.property('code', 0);
+      });
+
+      it('should log only errors to stderr', () =>
+        expect(this.result)
+          .to.have.property('stderr')
+          .that.contains(` ERROR: ${logs.error}`)
+          .and.does.not.contain(` DEBUG: ${logs.debug}`));
+      it('should log only warnings to stdout', () =>
+        expect(this.result)
+          .to.have.property('stdout')
+          .that.contains(` WARN: ${logs.warn}`)
+          .and.does.not.contain(` INFO: ${logs.info}`)
+          .and.does.not.contain(` VERBOSE: ${logs.verbose}`));
+    });
+
+    describe('DEBUG with the command line argument overriding the equivalent environment variable', function () {
+      before(() => {
+        this.result = shell.exec('SNOW_LOG_LEVEL=ERROR ./log-tester.js --log-level debug');
+        expect(this.result).to.include.property('code', 0);
+      });
+
+      it('should log errors and debug messages to stderr', () =>
+        expect(this.result)
+          .to.have.property('stderr')
+          .that.contains(` ERROR: ${logs.error}`)
+          .and.contains(` DEBUG: ${logs.debug}`));
+      it('should log warnings, info and verbose messages to stdout', () =>
+        expect(this.result)
+          .to.have.property('stdout')
+          .that.contains(` WARN: ${logs.warn}`)
+          .and.contains(` INFO: ${logs.info}`)
+          .and.contains(` VERBOSE: ${logs.verbose}`));
+    });
   });
 
   after('restore pwd', () => {
